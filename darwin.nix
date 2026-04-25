5d3dbc009f6785c447cfe9c11adeba64bb16b6d9
Travis Johnson <travis@thisguy.codes>
2026-04-25 16:29:28 -0700

    add ghostty cachix

--- darwin.nix
+++ darwin.nix
@@ -16,11 +16,13 @@
     substituters = [
       "https://devenv.cachix.org"
       "https://cache.nixos.org"
+      "https://ghostty.cachix.org"
     ];
     trusted-public-keys = [
       # this is the default one
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
     ];
   };
 
